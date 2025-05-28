9fc3857735b88c9d629ee2da3ef8cfea8797367e
punp2002 <punp2002@gmail.com>
2025-05-28 10:38:47 +0900

    server

---
+++ boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AdminServiceImpl.java
@@ -0,0 +1,35 @@
+package edu.kh.project.admin.model.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.project.admin.model.mapper.AdminMapper;
+import edu.kh.project.member.model.dto.Member;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {
+
+	private final AdminMapper mapper;
+	private final BCryptPasswordEncoder bcrypt;
+	
+	// 관리자 로그인
+	@Override
+	public Member login(Member inputMember) {
+		
+		Member loginMember = mapper.login(inputMember.getMemberEmail());
+		
+		if(loginMember == null) {
+			return null;
+		}
+		
+		if(!bcrypt.matches(inputMember.getMemberPw(), loginMember.getMemberPw())) {
+			return null;
+		}
+		loginMember.setMemberPw(null);
+		return loginMember;
+	}
+}
